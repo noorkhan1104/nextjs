d18dc7fc0867e56af909b3bd458c958aa86db7a8
noorkhan1104 <164567175+noorkhan1104@users.noreply.github.com>
2024-11-09 23:14:32 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+
+ import Link from "next/link";
+import Header from "./components/Header";
+
+export default function Home()  {
+  return (
+    <div>
+       <Header/>
+       
+    </div>
+  )
+}
